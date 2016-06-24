32f3bdb8c44f4692acb3d8624f607f85f700cdaa
Cristian Muscalu <muscaiu@yahoo.com>
2016-06-24 17:47:43 +0300

    implement a firebase list with add update delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,55 @@ import { AngularFire, FirebaseObjectObservable , FirebaseListObservable } from '
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html',
+  template: `
+<ul>
+  <li *ngFor="let item of items | async">
+    <input type="text" #updatesize [value]="item.text" />
+    <button (click)="update(item.$key, updatesize.value)">Update</button>
+    <button (click)="deleteItem(item.$key)">Delete</button>
+  </li>
+</ul>
+
+<input type="text" #newitem />
+<button (click)="add(newitem.value)">Add</button>
+<button (click)="deleteEverything()">Delete All</button>
+  `,
   styleUrls: ['app.component.css']
 })
+
 export class AppComponent {
-  item: FirebaseObjectObservable<any>;
+  items: FirebaseListObservable<any>;
   constructor(af: AngularFire) {
-    this.item = af.database.object('/item');
+    this.items = af.database.list('/messages');
   }
-  save(newName: string) {
-    this.item.set({ name: newName });
+  add(newName: string) {
+    this.items.push({ text: newName });
   }
-  update(newSize: string) {
-    this.item.update({ size: newSize });
+  update(key: string, newSize: string) {
+    this.items.update(key, { size: newSize });
   }
-  delete() {
-    this.item.remove();
+  deleteItem(key: string) {    
+    this.items.remove(key); 
+  }
+  deleteEverything() {
+    this.items.remove();
   }
 }
+//   item: FirebaseObjectObservable<any>;
+
+//   constructor(af: AngularFire) {
+//     this.item = af.database.object('/item');
+//     // this.item = af.database.object('/item', { preserveSnapshot: true });
+//     // this.item.subscribe(snapshot => console.log(snapshot.key()));
+//     //const absolute = af.database.list('https://<your-app>.firebaseio.com/items');
+//   }
+//   save(newName: string) {
+//     this.item.set({ name: newName });
+//   }
+//   update(newSize: string) {
+//     this.item.update({ size: newSize });
+//   }
+//   delete() {
+//     this.item.remove();
+//   }
+// }
