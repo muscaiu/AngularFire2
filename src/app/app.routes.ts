3eaa0b3c1b1fd59966da9938d6ac449c65c7a360
Laptop <Laptop>
2016-06-28 21:36:50 +0300

    removed test-route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,11 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 import {FirebaseListComponent} from './firebase-list/firebase-list.component';
 import {FormularComponent} from './formular/formular.component';
-import {TestRouteComponent} from './test-route/test-route.component';
 import {Profile} from './profile';
 
 export const routes: RouterConfig = [
    { path: 'firebase', component: FirebaseListComponent },   
-   { path: 'formular', component: FormularComponent },   
-   { path: 'testroute', component: TestRouteComponent },         
+   { path: 'formular', component: FormularComponent },           
    { path: 'profile', component: Profile },         
 //   { path: 'heroes', component: HeroListComponent },
 //   { path: 'hero/:id', component: HeroDetailComponent }
